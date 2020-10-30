9721e3e03494b76f565db7c3d7e5c3ae6a33885b
KteThl <kte.thaler@gmail.com>
2020-10-30 12:35:27 +0100

    first commit
    first commit yay

---
+++ js/loading.js
@@ -0,0 +1,7 @@
+setTimeout(function () {
+  document.querySelector("div.loading").classList.add("hidden")
+}, 4000)
+
+setTimeout(function () {
+  document.querySelector("div.switch").classList.add("hidden")
+}, 2000)
